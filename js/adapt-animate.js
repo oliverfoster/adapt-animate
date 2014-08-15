51e1e77e88659db04a5322883c3ede2e7277c539
Oliver Foster <oliver.foster@kineo.com>
2014-08-15 10:04:24 +0100

    Detached onscreen component from animate

--- js/adapt-animate.js
+++ js/adapt-animate.js
@@ -9,6 +9,7 @@ define(function(require) {
 	var Adapt = require('coreJS/adapt');
 	var Backbone = require('backbone');
 	var Emmet = require('extensions/adapt-animate/js/emmet.min');
+	var OnScreen = require('extensions/adapt-animate/js/onscreen');
 	var elements = [ "global", "contentObjects", "articles", "blocks", "components" ];
 	var config = undefined;
 
@@ -200,7 +201,7 @@ define(function(require) {
 					break;
 				case "outview":
 					function outviewCallback(event, onScreen) {
-						var onScreen = animate.element.getOnScreen($(event.currentTarget));
+						var onScreen = $(event.currentTarget).onscreen(); //animate.element.getOnScreen($(event.currentTarget));
 						var isInView = true;
 						if (onScreen.inviewP <= 0) isInView = false;
 
@@ -269,7 +270,7 @@ define(function(require) {
 							var index = (actionOn.action.direction == "backward" ? (elements.length - 1) - i : i);
 							var $element = $(elements[index]);
 
-							var onScreen = animate.element.getOnScreen($element);
+							var onScreen = $element.onscreen(); //animate.element.getOnScreen($element);
 							
 							var alt = animate.attach.expandContext(actionOn.alterations, eventsOn.groupByEvent.count, eventsOn.groupByOn.count, eventsOn.count, actionOn.count, index, i, onScreen);
 							var alterations = $(emmet.expandAbbreviation(alt,"plain"))[0];
@@ -355,60 +356,6 @@ define(function(require) {
 				string = string.replace(/\$b/g, onScreen.bottom);
 				string = string.replace(/\$l/g, onScreen.left);
 				return string;
-			}
-		},
-		element: {
-			getOnScreen: function($element) {
-				var height = $element.height();
-				var width = $element.width();
-				var wHeight = $(window).height();
-				var wWidth = $(window).width();
-				
-				//topleft from topleft of window
-				var top = $element.offset()["top"] - $(window).scrollTop();
-				var left = $element.offset()["left"] - $(window).scrollLeft();
-
-				//bottomright from bottomright of window
-				var bottom = wHeight - (top + height);
-				var right = wWidth - (left + width);
-				
-				//percentages of above
-				var topP = Math.round((100 /  wHeight) * top);
-				var leftP = Math.round((100 / wWidth) * left);
-				var bottomP = Math.round((100 /  wHeight) * (wHeight - (top + height)));
-				var rightP = Math.round((100 / wWidth) * (wWidth - (left + width)));
-
-
-				//inview
-				var inviewH = null;
-				if (left+width > 0 && right < 0 && left < 0) {
-					inviewH = width;
-				} else if (left < 0) { //offscreen left
-					inviewH = (width + left);
-				} else if (left + width > wWidth) { //offscreen right
-					inviewH = (wWidth - left);
-				} else { //fully inscreen
-					inviewH = width;
-				}
-
-				var inviewV = null;
-				if (top+height > 0 && bottom < 0 && top < 0) {
-					inviewV = height;
-				} else if (top < 0) { //offscreen top
-					inviewV = (height + top);
-				} else if (top + height > wHeight) { //offscreen bottom
-					inviewV = (wHeight - top);
-				} else { //fully inscreen
-					inviewV = height;
-				}
-
-				var area = height * width;
-				var inviewArea = inviewV * inviewH;
-				var inviewP = Math.round((100 / area) * inviewArea);
-
-				var uniq = ""+top+left+bottom+right+height+width+wHeight+wWidth;
-
-				return { top: top, left: left, bottom: bottom, right: right, topP: topP, leftP:leftP, bottomP: bottomP, rightP: rightP, inviewP:inviewP, uniq:uniq };
 			}
 		},
 		event: {
@@ -575,52 +522,5 @@ define(function(require) {
 	Adapt.on("componentView:postRender", function(view) {
 		animate.go(view, "components");
 	});
-
-
-
-
-	
-	function onscreen () {
-		var objs = _.filter($.cache, function(item) {
-			if (item.events !== undefined && item.events.onscreen !== undefined) return true;
-		});
-		if (objs.length === 0) {
-			//nothing to onscreen
-			clearInterval(onscreen.interval);
-			onscreen.timeslice = 1000;
-			onscreen.interval = setInterval(onscreen, onscreen.timeslice);
-		} else {
-			//something to onscreen
-			clearInterval(onscreen.interval);
-			onscreen.timeslice = 500;
-			onscreen.interval = setInterval(onscreen, onscreen.timeslice);
-		}
-
-		_.each(objs, function(obj) {
-			_.each(obj.events.onscreen, function(listener) {
-				var items = undefined;
-				if (listener.selector === undefined) {
-					items = $(obj.handle.elem);
-				} else {
-					items = $(obj.handle.elem).find(listener.selector);
-				}
-				_.each(items, function(item) {
-					$item = $(item);
-					var onscreen = animate.element.getOnScreen($item);
-					if (item.onscreen !== undefined && item.onscreen === onscreen.uniq) return;
-					item.onscreen = onscreen.uniq;
-					if (onscreen.inviewP > 0) {
-						$item.trigger("onscreen", onscreen);
-					} else {
-						
-						$item.trigger("onscreen", onscreen);
-					}
-				});
-			});
-		});
-
-	}
-	onscreen.timeslice = 333;
-	onscreen.interval = setInterval(onscreen, onscreen.timeslice);
 
 })
---
+++ js/onscreen.js
@@ -0,0 +1,100 @@
+(function() {
+	function getOnScreen($element) {
+		var height = $element.height();
+		var width = $element.width();
+		var wHeight = $(window).height();
+		var wWidth = $(window).width();
+		
+		//topleft from topleft of window
+		var top = $element.offset()["top"] - $(window).scrollTop();
+		var left = $element.offset()["left"] - $(window).scrollLeft();
+
+		//bottomright from bottomright of window
+		var bottom = wHeight - (top + height);
+		var right = wWidth - (left + width);
+		
+		//percentages of above
+		var topP = Math.round((100 /  wHeight) * top);
+		var leftP = Math.round((100 / wWidth) * left);
+		var bottomP = Math.round((100 /  wHeight) * (wHeight - (top + height)));
+		var rightP = Math.round((100 / wWidth) * (wWidth - (left + width)));
+
+
+		//inview
+		var inviewH = null;
+		if (left+width > 0 && right < 0 && left < 0) {
+			inviewH = width;
+		} else if (left < 0) { //offscreen left
+			inviewH = (width + left);
+		} else if (left + width > wWidth) { //offscreen right
+			inviewH = (wWidth - left);
+		} else { //fully inscreen
+			inviewH = width;
+		}
+
+		var inviewV = null;
+		if (top+height > 0 && bottom < 0 && top < 0) {
+			inviewV = height;
+		} else if (top < 0) { //offscreen top
+			inviewV = (height + top);
+		} else if (top + height > wHeight) { //offscreen bottom
+			inviewV = (wHeight - top);
+		} else { //fully inscreen
+			inviewV = height;
+		}
+
+		var area = height * width;
+		var inviewArea = inviewV * inviewH;
+		var inviewP = Math.round((100 / area) * inviewArea);
+
+		var uniq = ""+top+left+bottom+right+height+width+wHeight+wWidth;
+
+		return { top: top, left: left, bottom: bottom, right: right, topP: topP, leftP:leftP, bottomP: bottomP, rightP: rightP, inviewP:inviewP, uniq:uniq };
+	}
+	function onscreen () {
+		var objs = _.filter($.cache, function(item) {
+			if (item.events !== undefined && item.events.onscreen !== undefined) return true;
+		});
+		if (objs.length === 0) {
+			//nothing to onscreen
+			clearInterval(onscreen.interval);
+			onscreen.timeslice = 1000;
+			onscreen.interval = setInterval(onscreen, onscreen.timeslice);
+		} else {
+			//something to onscreen
+			clearInterval(onscreen.interval);
+			onscreen.timeslice = 500;
+			onscreen.interval = setInterval(onscreen, onscreen.timeslice);
+		}
+
+		_.each(objs, function(obj) {
+			_.each(obj.events.onscreen, function(listener) {
+				var items = undefined;
+				if (listener.selector === undefined) {
+					items = $(obj.handle.elem);
+				} else {
+					items = $(obj.handle.elem).find(listener.selector);
+				}
+				_.each(items, function(item) {
+					$item = $(item);
+					var onscreen = getOnScreen($item);
+					if (item.onscreen !== undefined && item.onscreen === onscreen.uniq) return;
+					item.onscreen = onscreen.uniq;
+					if (onscreen.inviewP > 0) {
+						$item.trigger("onscreen", onscreen);
+					} else {
+						$item.trigger("onscreen", onscreen);
+					}
+				});
+			});
+		});
+
+	}
+	onscreen.timeslice = 333;
+	onscreen.interval = setInterval(onscreen, onscreen.timeslice);
+	$.fn.onscreen = function() {
+		for (var i = 0; i < 1; i++ ) {
+			return getOnScreen($(this[i]));
+		}
+	};
+})();
